3dd174343c8ff219b261c78a0d14afdc3ad44e24
Michael George <michael_g_2@yahoo.co.uk>
2016-10-12 16:56:06 +0100

    added new conditions

---
+++ OtoscopeSimulator/Model/new_conditions.swift
@@ -0,0 +1,49 @@
+
+	//RED TYMPANIC MEMBRANE
+
+	static let BullousMyringitis = Condition(name: "Bullous Myringitis", thumbnailName: "bullous_myringitis.png", imageName: "bullous_myringitis.png", testConditionName: ["Acute Otitis Media", "Bullous Myringitis", "Glomus Tumour", "Haemotympanum"], informationText: "Myringitis means inflammation of the tympanic membrane. Blood or serous filled bullae form between the layers of the tympanic membrane causing sudden and severe pain", signs: "Blistering of the tympanic membrane", "Blisters can be filled with serous fluid or blood")
+
+	static let AcuteOtitisMedia1 = Condition(name: "Acute Otitis Media", thumbnailName: "acute_otitis_media.png", imageName: "acute_otitis_media.png", testConditionName: ["Acute Otitis Media", "Bullous Myringitis", "Glomus Tumour", "Haemotympanum"], informationText: "Acute otitis media is an infection of the middle ear. Typically characterised by a bulging tympanic membrane.", signs: ["Bulging of the tympanic membrane", "Increased vascularity / hyperaemia")
+
+	static let Haemotympanum = Condition(name: "Haemotympanum", thumbnailName: "haemotympanum.png", imageName: "haemotympanum.png", testConditionName: ["Acute Otitis Media", "Bullous Myringitis", "Glomus Tumour", "Haemotympanum"], informationText: "Haemotympanum can occur following trauma and can indicate a basal skull fracture. It is important to also check behind the ear lobe for bruising (Battle’s Sign).", signs: ["Dark red fluid collection behind the tympanic membrane", "Bulging of the tympanic membrane"])
+
+	static let Otosclerosis = Condition(name: "Otosclerosis", thumbnailName: "otosclerosis.png", imageName: "otosclerosis.png", testConditionName: ["Acute Otitis Media", "Bullous Myringitis", "Glomus Tumour", "Haemotympanum"], informationText: "Otosclerosis is a process by which the normal bony structure of stapes and / or cochlea is replaced by spongy and then sclerotic bone. This causes the stapes footplate to harden and limit its mobility. The pinkish color seen (known as schwartz sign) is increased vascularity of the cochlea seen during the spongy phase of the disease.", signs: ["Localised area of pinkish coloration behind the tympanic membrane"])
+
+	static let GlomusTumor = Condition(name: "Glomus Tumor", thumbnailName: "glomus_tumour.png", imageName: "glomus_tumour.png", testConditionName: ["Acute Otitis Media", "Bullous Myringitis", "Glomus Tumour", "Haemotympanum"], informationText: "", signs: ["Localised area of pinkish coloration behind the tympanic membrane"])
+
+
+	//WHITE TYMPANIC MEMBRANE
+
+	static let Tympanosclerosis = Condition(name: "Tympanosclerosis", thumbnailName: "tympanosclerosis.png", imageName: "tympanosclerosis.png", testConditionName: ["Tympanosclerosis", "Congenital Cholesteatoma", "Osteoma", "Otomycosis"], informationText: "Myringosclerosis is scarring of the tympanic membrane. Commonly occurs secondary to recurrent infections of the middle ear.", signs: ["Chalky white patches on the tympanic membrane"])
+
+	static let CongenitalCholesteatoma = Condition(name: "Congenital Cholesteatoma", thumbnailName: "congenital_cholesteatoma.png", imageName: "congenital_cholesteatoma.png", testConditionName: ["Tympanosclerosis", "Congenital Cholesteatoma", "Osteoma", "Otomycosis"], informationText: "", signs: ["Pearly mass behind the tympanic membrane"])
+
+	static let Osteoma = Condition(name: "Osteoma", thumbnailName: "osteoma.png", imageName: "osteoma.png", testConditionName: ["Tympanosclerosis", "Congenital Cholesteatoma", "Osteoma", "Otomycosis"], informationText: "", signs: ["Bony white outpouchings of the ear canal"])
+
+	static let Grommet1 = Condition(name: "Grommet", thumbnailName: "grommet.png", imageName: "grommet.png", testConditionName: ["Tympanosclerosis", "Congenital Cholesteatoma", "Osteoma", "Otomycosis"], informationText: "", signs: ["Grommet is usually placed antero-inferiorly"])
+
+	static let OtomycosisCandidal = Condition(name: "Otomycosis (Candidal)", thumbnailName: "otomycosis.png", imageName: "otomycosis.png", testConditionName: ["Tympanosclerosis", "Congenital Cholesteatoma", "Osteoma", "Otomycosis"], informationText: "", signs: ["Patchy white found on ear canal and sometimes the tympanic membrane"])
+
+	//COMMON
+
+	static let AcuteOtitisMedia2 = Condition(name: "Acute Otitis Media", thumbnailName: "acute_otitis_media.png", imageName: "acute_otitis_media.png", testConditionName: ["Acute Otitis Media", "Bullous Myringitis", "Glomus Tumour", "Haemotympanum"], informationText: "Acute otitis media is an infection of the middle ear. Typically characterised by a bulging tympanic membrane.", signs: ["Bulging of the tympanic membrane", "Increased vascularity / hyperaemia")
+
+	static let OtitisMediaWithEffusion = Condition(name: "Otitis media with effusion", thumbnailName: "otitis_media_with_effusion.png", imageName: "otitis_media_with_effusion.png", testConditionName: ["Acute otitis media", "Haemotympanum", "Ottic barotrauma", "Otitis media with effusion"], informationText: "Otitis media with effusion is a very common condition in which the mucosal lining of the middle ear secretes fluid which may occur as a result of eustachian tube dysfunction. The condition is also known as ‘Glue Ear’.", signs: ["Discolored tympanic membrane (can be amber, blue or yellow", "Retraction of the tympanic membrane (If mild the annulus will look more prominent"), "Air fluid level or bubbles behind the tympanic membrane"])
+
+	static let OtitisExterna = Condition(name: "Otitis Externa", thumbnailName: "otitis_externa.png", imageName: "otitis_externa.png", testConditionName: ["Acute Otitis Media", "Bullous Myringitis", "Glomus Tumour", "Haemotympanum"], informationText: "", signs: ["Ear canal red and oedematous", "Tympanic membrane sometimes not visible due to swelling")
+
+	static let TympanicMembranePerforation = Condition(name: "Tympanic Membrane Perforation", thumbnailName: "tm_perforation.png", imageName: "tm_perforation.png", testConditionName: ["Myringosclerosis", "Tympanic Membrane Perforation", "Haemotympanum", "Foreign Body"], informationText: "Tympanic membrane perforations can be caused by direct penetrating injury or from barotrauma - a sudden change in pressure.", signs: ["Perforation", "Blood suggests trauma"])
+
+	static let Grommet2 = Condition(name: "Grommet", thumbnailName: "grommet.png", imageName: "grommet.png", testConditionName: ["Tympanosclerosis", "Congenital Cholesteatoma", "Osteoma", "Otomycosis"], informationText: "", signs: ["Grommet is usually placed antero-inferiorly"])
+
+	//EMERGENCY
+
+	static let AcquiredCholesteatoma = Condition(name: "Acquired Cholesteatoma", thumbnailName: "acquired_cholesteatoma.png", imageName: "acquired_cholesteatoma.png", testConditionName: ["Acquired Cholesteatoma", "Congenital Cholesteatoma", "Malignant Otitis Externa", "Attic Retraction Pocket"], informationText: "", signs: ["Debris or keratin plug see in the attic (superior portion of the tympanic membrane"])
+
+	static let MalignantOtitisExterna1 = Condition(name: "Malignant Otitis Externa", thumbnailName: "malignant_oe.png", imageName: "malignant_oe.png", testConditionName: ["Acquired Cholesteatoma", "Congenital Cholesteatoma", "Malignant Otitis Externa", "Attic Retraction Pocket"], informationText: "", signs: ["Ear canal red and oedematous", "Granulation tissue"])
+
+	static let MalignantOtitisExterna2= Condition(name: "Malignant Otitis Externa", thumbnailName: "malignant_oe2.png", imageName: "malignant_oe.png", testConditionName: ["Acquired Cholesteatoma", "Congenital Cholesteatoma", "Malignant Otitis Externa", "Attic Retraction Pocket"], informationText: "", signs: ["Ear canal red and oedematous", "Granulation tissue"])
+
+	static let AtticRetraction1 = Condition(name: "Attic Retraction", thumbnailName: "tympanosclerosis_retraction_pocket.png", imageName: "tympanosclerosis_retraction_pocket.png", testConditionName: ["Acquired Cholesteatoma", "Congenital Cholesteatoma", "Malignant Otitis Externa", "Attic Retraction Pocket"], informationText: "", signs: ["Retraction pocket in attic (superior area of the tympanic membrane / pars flaccida)"])
+
+	static let AtticRetraction2 = Condition(name: "Attic Retraction", thumbnailName: "attic_retraction.png", imageName: "attic_retraction.png", testConditionName: ["Acquired Cholesteatoma", "Congenital Cholesteatoma", "Malignant Otitis Externa", "Attic Retraction Pocket"], informationText: "", signs: ["Retraction pocket in attic (superior area of the tympanic membrane / pars flaccida)"])
