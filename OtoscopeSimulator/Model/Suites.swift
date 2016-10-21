1c40c6f32c6c69f97e6c98e9692e84df99b8d69f
John Holcroft <john.holcroft@montreux.co.uk>
2016-10-21 14:01:32 +0100

    Updated all content to expected final version.

--- OtoscopeSimulator/Model/Suites.swift
+++ OtoscopeSimulator/Model/Suites.swift
@@ -4,9 +4,9 @@ struct Suites {
     static let CommonConditions = Suite(name:"5 Common Conditions", htmlDescription:"", conditions:Conditions.Common)
     
     static let EmergencyConditions = Suite(name:"Emergency and life threatening", htmlDescription:"", conditions:Conditions.Emergency)
-    static let NormalOrNotNormalConditions = Suite(name:"Normal or not normal", htmlDescription:"xxx", conditions:[])
-    static let RedTympanicMembraneConditions = Suite(name:"Things that are red", htmlDescription:"xxx", conditions:Conditions.RedTympanicMembrane)
-    static let WhiteTympanicMembraneConditions = Suite(name:"Things that are white", htmlDescription:"xxx", conditions:Conditions.WhiteTympanicMembrane)
+    static let NormalOrNotNormalConditions = Suite(name:"Normal or not normal", htmlDescription:"", conditions:[])
+    static let RedTympanicMembraneConditions = Suite(name:"The red tympanic membrane", htmlDescription:"", conditions:Conditions.RedTympanicMembrane)
+    static let WhiteTympanicMembraneConditions = Suite(name:"The white tympanic membrane", htmlDescription:"", conditions:Conditions.WhiteTympanicMembrane)
     
     static let TestSet = [CommonConditions, EmergencyConditions, NormalOrNotNormalConditions, RedTympanicMembraneConditions, WhiteTympanicMembraneConditions]
     
